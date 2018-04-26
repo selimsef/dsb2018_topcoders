244621645b69c42ee08350861185f7a7f08e2fd7
Alexander Buslaev <al.buslaev@gmail.com>
2018-04-26 22:37:00 +0300

    upd setup_env file

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
 
+echo 'export PATH=/opt/conda/bin:$PATH' > /etc/profile.d/conda.sh
+wget --quiet https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh -O ~/anaconda.sh
+/bin/bash ~/anaconda.sh -b -p /opt/conda
+rm ~/anaconda.sh
+export PATH=/opt/conda/bin:$PATH
+pip install --upgrade tensorflow-gpu
+pip install --upgrade keras
+pip install imgaug
+conda install opencv
 
-
-# todo all previous (install anaconda)
-pip install http://download.pytorch.org/whl/cu90/torch-0.3.1-cp36-cp36m-linux_x86_64.whl 
+pip install http://download.pytorch.org/whl/cu90/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
 pip install torchvision tensorboardX
