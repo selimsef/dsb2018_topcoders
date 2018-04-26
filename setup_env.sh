194111ea69330f8b1f6318b6986dc1de06f40c4e
Alexander Buslaev <al.buslaev@gmail.com>
2018-04-26 22:41:37 +0300

    add lightgbm and tqdm

--- setup_env.sh
+++ setup_env.sh
@@ -7,8 +7,8 @@ rm ~/anaconda.sh
 export PATH=/opt/conda/bin:$PATH
 pip install --upgrade tensorflow-gpu
 pip install --upgrade keras
-pip install imgaug
-conda install opencv
+pip install imgaug lightgbm
+conda install opencv tqdm
 
 pip install http://download.pytorch.org/whl/cu90/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
 pip install torchvision tensorboardX
